c697a01e3736ee5e7057dc1c8ac204135a14ee16
Saichaamar <168137500+Saichaamar@users.noreply.github.com>
2024-04-28 23:47:09 +0000

    Add files via upload

---
+++ student.controller.js
@@ -0,0 +1,81 @@
+const db = require("../models");
+const Student = db.students;
+
+// Create and Save a new Student
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.name) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+    // Create a Student
+    const student = new Student({
+        name: req.body.name,
+        course: req.body.course,
+        registered: req.body.registered ? req.body.registered : false,
+    });
+
+    // Save Student in the database
+    student
+        .save(student)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating the Student.",
+            });
+        });
+};
+
+// Retrieve all Students from the database.
+exports.findAll = (req, res) => {
+    const name = req.query.name;
+    let condition = name ? { name: { $regex: new RegExp(name), $options: "i" } } : {};
+    Student.find(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving students.",
+            });
+        });
+};
+
+//code de la fonction delete :
+// Delete a Student with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    Student.findByIdAndDelete(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete Student with id=${id}. Maybe Student was not found!`,
+                });
+            } else {
+                res.send({
+                    message: "Student was deleted successfully!",
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Student with id=" + id,
+            });
+        });
+};
+// Delete all Students from the database.
+exports.deleteAll = (req, res) => {
+    Student.deleteMany({})
+        .then(data => {
+            res.send({
+                message: `${data.deletedCount} Students were deleted successfully!`,
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while removing all students.",
+            });
+        });
+};
---
+++ student.routes.js
@@ -0,0 +1,19 @@
+module.exports = app => {
+    const students = require("../controllers/student.controller.js");
+    let router = require("express").Router();
+
+    // Create a new Student
+    router.post("/", students.create);
+
+    // Retrieve all Students
+    router.get("/", students.findAll);
+
+    app.use("/api/students", router);
+
+    // Delete a Student with id
+    router.delete("/:id", students.delete);
+
+    // Delete all students
+    router.delete("/", students.deleteAll);
+    app.use("/api/students", router);
+};
